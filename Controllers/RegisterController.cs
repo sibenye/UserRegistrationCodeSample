8e296b962d37e9916280717c54c2239403e39794
sibenye <sibenye@gmail.com>
2014-11-13 09:56:07 -0500

    added register view

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using UserRegistrationSample.Models;
+using UserRegistrationSample.Repositories;
+
+namespace UserRegistrationSample.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Register";
+            return View();
+        }
+    }
+}
